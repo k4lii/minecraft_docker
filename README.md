50b88b1cade899fc0a83fd4e14465f2d0366913d
lorris hamdaoui <lorris.hamdaoui@epitech.eu>
2022-12-03 00:21:33 +0100

    vanilla docker is working

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # minecraft_docker
+
+
+docker build -f Dockerfile.vanilla  --build-arg  VANILLA_VERSION=1.19.2 --build-arg PORT=5000 -t mc .
+
+docker run  --env MC_VERSION=1.19.2 PORT=6000 -p 6000 -it -d mc
+
+
+docker exec -it 17ef36e8b8a2 bash
---
+++ entrypoint_vanilla.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#auto download minecraft server for the choosen version
+curl -O $(curl -s $(curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -j '.["versions"][] | select(.id=="1.19.2").url') | jq -j '.["downloads"].server.url')
+
+if [ server.properties ]
+then
+    echo 'INFO: server.properties already exist'
+    echo "eula=true" > eula.txt
+else
+    java -jar server.jar --nogui --initSettings  && sed -i 's/eula=false/eula=true/g' eula.txt
+    echo 'WARN: server.properties doesnt exist, a blank one will be generated'
+fi
+echo 'INFO: EULA ACCEPTED'
+
+java -jar server.jar --nogui --port $PORT
