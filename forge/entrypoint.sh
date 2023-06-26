01656bf0f402a0e57ee80fe0c5d02673d7ad8154
Lorris <lorris.hamdaoui@gmail.com>
2023-06-26 16:15:58 +0000

    update

--- forge/entrypoint.sh
+++ forge/entrypoint.sh
@@ -3,17 +3,16 @@
 # #download minecraft server for choosen version on forge
 curl -O https://maven.minecraftforge.net/net/minecraftforge/forge/$MC_VERSION/forge-$MC_VERSION-installer.jar
 mv *.jar server.jar
-tail -f /dev/null
-# java -jar server.jar --installServer
-# ./run.sh && sed -i 's/eula=false/eula=true/g' eula.txt
-# echo 'INFO: EULA ACCEPTED'
+java -jar server.jar --installServer
+./run.sh && sed -i 's/eula=false/eula=true/g' eula.txt
+echo 'INFO: EULA ACCEPTED'
 
-# if [ server.properties ]
-# then
-#     echo 'INFO: server.properties already exist'
-# else
-#     echo 'WARN: server.properties doesnt exist, a blank one will be generated'
-# fi
+if [ server.properties ]
+then
+     echo 'INFO: server.properties already exist'
+else
+     echo 'WARN: server.properties doesnt exist, a blank one will be generated'
+fi
 
 # if [ "$BACKUP" == "true" ] ;
 # then
