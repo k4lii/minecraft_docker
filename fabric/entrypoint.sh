df7bb4e364adb1ce1fae58f25970c346db424fcc
VMserverMC <lorris.hamdaoui@gmail.com>
2023-06-27 23:42:58 +0000

    uodate

--- fabric/entrypoint.sh
+++ fabric/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+if [ -f server.properties ]
+then
+    echo 'INFO: server.properties already exist'
+else
+    echo 'WARN: server.properties doesnt exist, a blank one will be generated'
+    wget https://mediafilez.forgecdn.net/files/4329/571/Better+MC+FABRIC+Server+Pack+1.19.2+v13.5.zip
+    unzip Better+MC+FABRIC+Server+Pack+1.19.2+v13.5.zip
+    rm Better+MC+FABRIC+Server+Pack+1.19.2+v13.5.zip
+    cp -r Better\ MC\ FABRIC\ Server\ Pack\ 1.19.2/* .
+    rm -rf Better\ MC\ FABRIC\ Server\ Pack\ 1.19.2/
+    echo 'eula=true' > eula.txt
+    chmod +x start.sh
+    ./start.sh
+fi
 
-wget https://mediafilez.forgecdn.net/files/4560/176/Better+MC+Server+Pack+FABRIC+1.19.2+v18.5.zip
-
-tail -f /dev/null
+./start.sh
