f971fc233e6349dfe56511cd14c4c997a79181c9
VMserverMC <lorris.hamdaoui@gmail.com>
2023-06-27 23:01:35 +0000

    update

--- fabric/entrypoint.sh
+++ fabric/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-# wget https://mediafilez.forgecdn.net/files/4560/176/Better+MC+Server+Pack+FABRIC+1.19.2+v18.5.zip
+wget https://mediafilez.forgecdn.net/files/4560/176/Better+MC+Server+Pack+FABRIC+1.19.2+v18.5.zip
 
 tail -f /dev/null
